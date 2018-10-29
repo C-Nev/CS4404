121a61d78a67ae5f3305c6192bf4adffe745f0f3
Eoin Roche <rochee6@tcd.ie>
2018-10-28 18:45:27 -0700

    added optimization code to linear regression in Compare file

--- linearRegression.py
+++ linearRegression.py
@@ -4,17 +4,16 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
-def main():
-    data = pd.read_csv('someFile')
+def learnAndPrintResults(dataSet):
     (m, n) = data.shape
-    feature_cols = ["List with" , "column titles"]
-    X = data[feature_cols]
-    Y = data["Y Column"]
+    X = dataSet.data
+    y = dataSet.target
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
     linreg = LinearRegression()
     linreg.fit(X_train, y_train)
-    ypred = linreg.pred(X_test)
+    ypred = linreg.predict(X_test)
 
     absoluteErr = metrics.mean_absolute_error(y_test, ypred)
-    squaredErr = metrics.mean_square_error(y_test, ypred)
-    rootErr = np.sqrt(squaredErr)
+    #squaredErr = metrics.mean_square_error(y_test, ypred)
+    #rootErr = np.sqrt(squaredErr)
+    print(absoluteErr
